bd57d588b7562bd09a273356bc1defef64dd8709
itsGBN <nem8887@nyu.edu>
2023-09-25 18:30:06 -0400

    Finished Player Radgoll Physics

--- Assets/Scripts/MoveBody.cs
+++ Assets/Scripts/MoveBody.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveBody : MonoBehaviour
+public class MoveBody
+    : MonoBehaviour
 {
     Rigidbody2D myBody;
-    float bodyPower = 20f;
+    float bodyPower = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,7 @@ public class MoveBody : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A)) 
+        if ((Input.GetKey(KeyCode.A) || (Input.GetKey(KeyCode.D))))
         {
             myBody.AddForce(transform.up * bodyPower, ForceMode2D.Impulse);
         }
---
+++ Assets/Scripts/MoveLeftArm.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftArm
+    : MonoBehaviour
+{
+    Rigidbody2D myArm;
+    float armPower = 1f;
+    Vector3 arm = new Vector3(0, -2, 0);
+    Vector3 armdown = new Vector3(0, 2, 0);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myArm = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            myArm.AddForce(arm * armPower, ForceMode2D.Impulse);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            myArm.AddForce(armdown * armPower, ForceMode2D.Impulse);
+        }
+    }
+}
